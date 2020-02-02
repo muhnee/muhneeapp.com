0658f869747265b3c39b67a06faa377b4ad11458
Eric Jiang <ericjiang97@gmail.com>
2020-02-02 13:35:15 +1100

    new tagline

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,9 +26,7 @@ export default () => (
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">Muhnee</h1>
-          <p className="text-xl lg:text-2xl mt-6 font-light">
-            An exciting and simple way to help you manage your money.
-          </p>
+          <p className="text-xl lg:text-2xl mt-6 font-light">Personal Finance, Simplified.</p>
           <p
             className="mt-8 md:mt-12"
             style={{
